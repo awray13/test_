194a86737d5b4da56260fd494e5c7918e665a588
Ashton Wray <aricks1986@gmail.com>
2021-09-06 17:30:57 -0400

    changes to test.py

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
 import pygame
+from pygame.locals import *
+
+
+pygame.init()
+
+
+
+pygame.display.update()
+
+
+
+while True:
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
